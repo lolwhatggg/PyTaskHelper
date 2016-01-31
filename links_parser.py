b96fb2375c8bd3cc4ea08e30b77d1164798bbfe6
Slava Vikharev <slavavicharev@gmail.com>
2016-01-31 14:25:18 +0500

    fap fap

--- links_parser.py
+++ links_parser.py
@@ -1,16 +1,13 @@
 import re
 from bs4 import BeautifulSoup
-from contextlib import closing
 from urllib.request import urlopen
 
-URL = 'http://anytask.urgu.org'
 NAMES = 'Perltask', 'python.task'
 
-with closing(urlopen(URL)) as page:
+with urlopen('http://anytask.urgu.org') as page:
     raw_html = page.read().decode()
 
 links = BeautifulSoup(raw_html).find_all('li')
-filtered = filter(lambda x: any(name in x.text for name in NAMES), links)
 
-for li in filtered:
-    print('%s: %s' % (re.sub(r'\s+', '', li.text), URL + li.a['href']))
+for link in filter(lambda x: any(name in x.text for name in NAMES), links):
+    print('%s: %s' % (re.sub(r'\s+', '', link.text), URL + link.a['href']))
