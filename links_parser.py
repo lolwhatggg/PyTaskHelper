9c89a043c51675fc90fea681bbdfcb8af297f0c7
Slava Vikharev <slavavicharev@gmail.com>
2016-01-31 14:28:29 +0500

    fuck...

--- links_parser.py
+++ links_parser.py
@@ -2,12 +2,13 @@ import re
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+URL = 'http://anytask.urgu.org'
 NAMES = 'Perltask', 'python.task'
 
-with urlopen('http://anytask.urgu.org') as page:
+with urlopen(URL) as page:
     raw_html = page.read().decode()
 
 links = BeautifulSoup(raw_html).find_all('li')
 
-for link in filter(lambda x: any(name in x.text for name in NAMES), links):
-    print('%s: %s' % (re.sub(r'\s+', '', link.text), URL + link.a['href']))
+for li in filter(lambda x: any(name in x.text for name in NAMES), links):
+    print('%s: %s' % (re.sub(r'\s+', '', li.text), URL + li.a['href']))
