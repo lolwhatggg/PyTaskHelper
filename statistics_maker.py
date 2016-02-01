b5d51d002653faeb4b56c58b14e32cdf58b941c6
SlavaVikharev <slavavicharev@gmail.com>
2016-02-02 01:31:02 +0500

    merging problems

--- statistics_maker.py
+++ statistics_maker.py
@@ -1,14 +1,9 @@
 import json
 import glob
 import os.path
-<<<<<<< HEAD
-import db_handlers as dbh
 from pprint import pprint, pformat
-
-=======
 import database
-import pprint
->>>>>>> 32ce2dc6514aba10abfc4a021837db9712587f4d
+
 
 def build_task_database(files, handler):
     task_db = database.Database(handler)
@@ -23,12 +18,6 @@ def build_task_database(files, handler):
 
 if __name__ == '__main__':
     files = glob.glob(os.path.join('courses', '*.json'))
-<<<<<<< HEAD
-    db = build_task_database(files, dbh.EntryWithoutAnnotations)
-    for task in sorted(db):
-        print('%s:\n%r\n' % (task, db[task]))
-=======
     task_db = build_task_database(files, database.EntryWithPercentage)
     for task in sorted(task_db):
-        pprint.pprint('{}: {}'.format(task, task_db[task]))
->>>>>>> 32ce2dc6514aba10abfc4a021837db9712587f4d
+        print('%s:\n%r\n' % (task, task_db[task]))
