d7468cfa9fdbb7eaaf3627bcddc7b5c9912acba9
SlavaVikharev <slavavicharev@gmail.com>
2016-02-01 23:08:44 +0500

    Fckn Sheet

--- db_handlers.py
+++ db_handlers.py
@@ -1,84 +1,53 @@
 from statistics import mean
+from inspect import getmembers
+from abc import ABCMeta, abstractmethod
 
 
 class Database(dict):
-    def __init__(self, handler):
+    def __init__(self, entry_class):
         super().__init__()
-        self.handler = handler
+        self._entry_class = entry_class
 
-    def add_entry(self, entry):
-        name = entry['name']
-        if name in self:
-            self[name] = self.handler.update_entry(self[name], entry)
-        else:
-            self[name] = self.handler.get_new_entry(entry)
+    def add_entry(self, data):
+        name = data['name']
+        if name not in self:
+            self[name] = self._entry_class(data)
+        self[name].update(data)
 
     def finalize(self):
-        for field in self:
-            self[field] = self.handler.get_finalized(self[field])
+        for name in self:
+            self[name].finalize()
 
 
-class Field:
-    def __init__(self, name, update_method, default_value):
-        self.name = name
-        self.update_method = update_method
-        self.default_value = default_value
+class Entry(metaclass=ABCMeta):
+    def __str__(self):
+        return str(self.__dict__)
+
+    @abstractmethod
+    def finalize(self):
+        pass
+
+    def update(self, new_data):
+        updaters = filter(lambda m: m[0].startswith('update_'),
+                          getmembers(self))
+        for name, updater in updaters:
+            updater(new_data)
 
 
-class DBHandler:
-    def __init__(self):
-        self.fields = []
+class EntryWithoutAnnotations(Entry):
+    def __init__(self, data):
+        self.middle = 0
+        self.name = data['name']
+        self.max = {data['max']}
+        self.points = [student['points'] for
+                       student in data['students']]
 
-    def update_entry(self, old_data, new_data):
-        result = {}
-        for field in self.fields:
-            if field.update_method is not None:
-                result[field.name] = field.update_method(old_data, new_data)
-            else:
-                result[field.name] = old_data[field.name]
-        return result
+    def update_points(self, new_data):
+        self.points += [student['points'] for student
+                        in new_data['students']]
 
-    def get_new_entry(self, entry):
-        result = {}
-        for field in self.fields:
-            result[field.name] = field.default_value(entry)
-        return result
+    def update_max(self, new_data):
+        self.max.add(new_data['max'])
 
-
-class DBHandlerWithoutAnnotations(DBHandler):
-    def __init__(self):
-        super().__init__()
-        self.fields = [
-            Field('name', None, Task.get_name),
-            Field('points', Task.update_points, Task.get_points),
-            Field('max', Task.update_max, Task.get_max)
-        ]
-
-    @staticmethod
-    def get_finalized(entry):
-        if entry['points']:
-            entry['middle'] = mean(entry['points'])
-        return entry
-
-
-class Task:
-    @staticmethod
-    def get_name(task):
-        return task['name']
-
-    @staticmethod
-    def get_points(task):
-        return [student['points'] for student
-                in task['students']]
-
-    @staticmethod
-    def get_max(task):
-        return {task['max']}
-
-    @staticmethod
-    def update_points(task, another_task):
-        return task['points'] + Task.get_points(another_task)
-
-    @staticmethod
-    def update_max(task, another_task):
-        return task['max'].union(Task.get_max(another_task))
+    def finalize(self):
+        self.middle = mean(self.points or [0])
--- statistics_maker.py
+++ statistics_maker.py
@@ -1,7 +1,7 @@
 import json
 import glob
 import os.path
-from db_handlers import DBHandlerWithoutAnnotations, Database
+from db_handlers import EntryWithoutAnnotations, Database
 
 KNOWN_ALIASES = {}
 
@@ -9,16 +9,16 @@ KNOWN_ALIASES = {}
 def build_task_database(files, handler):
     database = Database(handler)
     for filename in files:
-        with open(filename, 'r', encoding='utf-8') as file:
+        with open(filename, encoding='utf-8') as file:
             data = json.load(file)
-            for task in data:
-                database.add_entry(task)
+        for task in data:
+            database.add_entry(task)
     database.finalize()
     return database
 
 
 if __name__ == '__main__':
     files = glob.glob(os.path.join('courses', '*.json'))
-    db = build_task_database(files, DBHandlerWithoutAnnotations())
+    db = build_task_database(files, EntryWithoutAnnotations)
     for task in sorted(db):
         print('{}: {}'.format(task, db[task]))
