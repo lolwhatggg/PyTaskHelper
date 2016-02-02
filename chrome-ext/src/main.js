5d752b55dd1e85911e9a7523267e8c69e85057d7
slavavikharev <slavavicharev@gmail.com>
2016-02-03 03:02:20 +0500

    A bit modified

--- chrome-ext/src/main.js
+++ chrome-ext/src/main.js
@@ -1,35 +1,26 @@
 function process_json(data) {
-    var task_values = [];
-    $.each(data, function (name, elem) {
-        task_values[name] = elem['average']
-    });
-    console.log(task_values);
-    var current_tasks = [];
-    $('strong, font').each(function (k, v) {
+    $('strong, font').each(function(k, v) {
         var name = $(this).text();
-        var average = get_average(name, task_values);
-        if (average)
-        {
-            $(this).next('*').after(' Средний балл за все года: <span class="label  label-success">' +
-                                     average.toFixed(2).toString() +
-                                     '</span>');
+        if (!(name in data)) {
+            return;
         }
+        var text =
+            '&nbsp;Средний балл:&nbsp;' +
+            '<span class="label label-success">' +
+            data[name]['average'].toFixed(1) +
+            '</span>';
+        $(this).next('*').after(text);
+        $(this).wrap('<a href="#"></a>');
     });
 }
-function get_average(name, db){
-    if (name in db) {
-        return db[name];
-    }
-    else{
-        return null;
-    }
-}
+
 var xhr = new XMLHttpRequest();
-xhr.onload = function () {
+
+xhr.onload = function() {
     var json = xhr.responseText;
     data = JSON.parse(json);
-    process_json(data)
+    process_json(data);
 };
+
 xhr.open('GET', 'http://avefablo.xyz/db.json');
 xhr.send();
-
