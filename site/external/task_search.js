f491a453a041545a4f9d2ef3dcecc6a4396bc63c
avefablo <avef96@gmail.com>
2016-02-07 16:22:46 +0500

    поменял все на searchable исправил скрипт поиска

--- site/external/task_search.js
+++ site/external/task_search.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-    $("#filter").keyup(function () {
+    $("#filter").keyup(function (e) {
+        if (e.keyCode == 13)
+            return;
         var filter = $(this).val().toLowerCase();
         $(".task").each(function () {
-            if ($(this).find('.name').text().toLowerCase().indexOf(filter) != -1 ||
-                $(this).find('.category').text().toLowerCase().indexOf(filter) != -1) {
-                $(this).show();
-            } else {
-                $(this).fadeOut();
-            }
+            var show = false;
+            $(this).find('.searchable').each(function () {
+                show |= $(this).text().toLowerCase().indexOf(filter) != -1
+            });
+            show ? $(this).show() : $(this).fadeOut();
         });
     });
 });
