aaab045d219083732b76122ae1579b9399dccf75
avefablo <avef96@gmail.com>
2016-02-08 14:39:44 +0500

    clean up!

--- site/dist/js/auto-scroll.js
+++
@@ -1,6 +0,0 @@
-{% if is_mobile % }
-  // Hides mobile browser's address bar when page is done loading.
-  window.addEventListener('load', function(e) {
-    setTimeout(function() { window.scrollTo(0, 1); }, 1);
-  }, false);
-{% endif % }
