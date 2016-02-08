8d535fa9c8f1fd53d989d743dc75bb6c863a1df3
slavavikharev <slavavicharev@gmail.com>
2016-02-08 15:45:57 +0500

    Dunno what here

--- site/external/auto-scroll.js
+++ site/external/auto-scroll.js
@@ -1,6 +1,5 @@
-{% if is_mobile % }
-  // Hides mobile browser's address bar when page is done loading.
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   window.addEventListener('load', function(e) {
     setTimeout(function() { window.scrollTo(0, 1); }, 1);
   }, false);
-{% endif % }
+}
